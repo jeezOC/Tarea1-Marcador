5c13663eebb60cffde1c1bed1c76fc234cb4421b
jeezOC <82684698+jeezOC@users.noreply.github.com>
2021-10-07 19:47:04 -0600

    update

--- src/main/java/cr/ac/una/marcador/controller/mantMarcasController.java
+++ src/main/java/cr/ac/una/marcador/controller/mantMarcasController.java
@@ -66,34 +66,48 @@ public class mantMarcasController extends Controller implements Initializable {
     @FXML
     private ListView<String> listMarcas;
     
+    @FXML
+    private JFXButton btnBorrar;
+
+    @FXML
+    private JFXButton btnNuevo;
+
+    @FXML
+    private JFXButton btnGuardar;
+
+ 
+    
     /**
      * Initializes the controller class.
      */
     
-     List<MarcaDto> listdto=null;
-      EmpleadoDto emp=new EmpleadoDto();
+    List<MarcaDto> listdto=null;
+    EmpleadoDto emp = new EmpleadoDto();
      
      
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-            listMarcas.getItems().add("ENTRADAS/SALIDAS");
+            listMarcas.getItems().add("Entradas");
+            listMarcas.getItems().add("SALIDAS");
+            BtnExcel.setDisable(true);
     }    
     
     @FXML
     private void onAction_btnBuscar(ActionEvent event) {   
        
         String folio = txtBuscar.getText();
-        if(!"".equals(txtBuscar.getText())) {
-            
+        if(!folio.equals("")) {
             emp = wsConsumer.getInstance().buscarEmpleadoFolio(txtBuscar.getText());
             listdto = wsConsumer.getInstance().buscarMarcasFolioFechas(folio);
-            if(listdto!=null){
-            listdto.forEach(x -> {listMarcas.getItems().add(x.getMarcahoraEntrada().toString()+"/"+x.getMarcahoraSalida().toString());});
+            if(listdto.isEmpty()){
+                listdto.forEach(x -> {listMarcas.getItems().add(x.getMarcahoraEntrada().toString()+"/"+x.getMarcahoraSalida().toString());});
+                BtnExcel.setDisable(false);
             }else{
                 new Mensaje().showModal(Alert.AlertType.ERROR, "Datos insuficientes" ,this.getStage(),"Empleado ingresado no posee marcas.");
             }
+        }else{
+            new Mensaje().showModal(Alert.AlertType.ERROR, "Datos insuficientes" ,this.getStage(),"Favor ingrese el dato a buscar");
         }
-        else new Mensaje().showModal(Alert.AlertType.ERROR, "Datos insuficientes" ,this.getStage(),"Favor ingrese el dato a buscar");
         
         
         if(!"".equals(folio)&&folio.length()==7){
@@ -132,11 +146,8 @@ public class mantMarcasController extends Controller implements Initializable {
                 sheet.autoSizeColumn(300);
                 Cell cell;
                 Row row = sheet.createRow(0);
-                
                 style.setFillForegroundColor(IndexedColors.GREEN.getIndex());
                 style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-              //  row.setRowStyle(style);
-                
                 for (int i = 0 ;i < 7;i++){//cabeceras
                     cell = row.createCell(i);
                     cell.setCellStyle(style);
@@ -146,88 +157,57 @@ public class mantMarcasController extends Controller implements Initializable {
                     style.setBorderRight(BorderStyle.MEDIUM);           
                     cell.setCellValue(ValueOfHeader(i));
                 }
-                 XSSFCellStyle style2 = (XSSFCellStyle) wb.createCellStyle();
+                XSSFCellStyle style2 = (XSSFCellStyle) wb.createCellStyle();
                 style2.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
                 style2.setFillPattern(FillPatternType.SOLID_FOREGROUND);
                 
                 if(!listdto.isEmpty()){
-              for (int i= 1; i < listdto.size(); i++) {    
-                    row = sheet.createRow(i);
-                 for (int k = 1; k <= 7; k++) {   
-                    cell = row.createCell(k-1); 
-                    cell.setCellStyle(style2);
-                    style2.setBorderTop(BorderStyle.MEDIUM);
-                    style2.setBorderBottom(BorderStyle.MEDIUM);
-                    style2.setBorderLeft(BorderStyle.MEDIUM);
-                    style2.setBorderRight(BorderStyle.MEDIUM);                    
-                    style2.setAlignment(HorizontalAlignment.CENTER);
-                 if(k == 1) cell.setCellValue(emp.getFolio());  
-                    
-                 if(k == 2) cell.setCellValue(emp.getCedula());
-                   
-                 if(k == 3) cell.setCellValue(emp.getNombre());
-                   
-                 if(k == 4) cell.setCellValue(emp.getApellido());
-   
-                 if(k == 5){
-//                     CellStyle  cellStyle= wb.createCellStyle();
-                     style2.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy"));
-                     
-                     cell.setCellValue(listdto.get(i).getMarcajornada());
-                     cell.setCellStyle(style2);
-//                     CellUtil.setCellStyleProperty(cell, CellUtil.WRAP_TEXT, true);
-//                     cell.getRow().setHeight((short)-1);
-                     
-                 }
-                 if(k == 6) {
-//                     CellStyle  cellStyle= wb.createCellStyle();
-                     style2.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
-                     
-                     cell.setCellValue((LocalDate) listdto.get(i).getMarcahoraEntrada());
-                     cell.setCellStyle(style2);
-//                     CellUtil.setCellStyleProperty(cell, CellUtil.WRAP_TEXT, true);
-//                     cell.getRow().setHeight((short)-1);
-                   
-                 
-                 }
-                 if(k == 7) {
-//                     CellStyle  cellStyle= wb.createCellStyle();
-                     style2.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
-                     cell.setCellValue((LocalDate) listdto.get(i).getMarcahoraSalida());
-                     cell.setCellStyle(style2);
-//                     CellUtil.setCellStyleProperty(cell, CellUtil.WRAP_TEXT, true);
-//                     cell.getRow().setHeight((short)-1);
-                     
-                 }
-                 sheet.setColumnWidth(k, 4500);
-                 if(k!=1){
-//                     style.
-//                    cell.
-                 }
-//                 row.setHeight((short)-1);
+                    for (int i= 1; i < listdto.size(); i++) {    
+                        row = sheet.createRow(i);
+                        for (int k = 1; k <= 7; k++) {   
+                            cell = row.createCell(k-1); 
+                            cell.setCellStyle(style2);
+                            style2.setBorderTop(BorderStyle.MEDIUM);
+                            style2.setBorderBottom(BorderStyle.MEDIUM);
+                            style2.setBorderLeft(BorderStyle.MEDIUM);
+                            style2.setBorderRight(BorderStyle.MEDIUM);                    
+                            style2.setAlignment(HorizontalAlignment.CENTER);
+                            if(k == 1) cell.setCellValue(emp.getFolio());  
 
-//                sheet.autoSizeColumn(k);
-                 }
-                 
-              }
-            }
-//                wb.get
-//                IWorksheet worksheet = workbook.getWorksheets().get(0);
-                //set row height for row 1:2.
-//worksheet.getRange("1:2").setRowHeight(50);
-//
-////set column width for column C:D.
-//worksheet.getRange("C:D").setColumnWidth(20);   
-                
-//                autoSizeColumns(wb);
+                            if(k == 2) cell.setCellValue(emp.getCedula());
+
+                            if(k == 3) cell.setCellValue(emp.getNombre());
+
+                            if(k == 4) cell.setCellValue(emp.getApellido());
+
+                            if(k == 5){
+                               style2.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy"));
+                               cell.setCellValue(listdto.get(i).getMarcajornada());
+                            }
+                            if(k == 6) {
+                               style2.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
+                               cell.setCellValue((LocalDate) listdto.get(i).getMarcahoraEntrada());
+                               cell.setCellStyle(style2);
+                            }
+                            if(k == 7) {
+                               style2.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
+                               cell.setCellValue((LocalDate) listdto.get(i).getMarcahoraSalida());
+                               cell.setCellStyle(style2);
+
+                            }
+                            sheet.setColumnWidth(k, 4500);
+                            }
+                    }
+                }
                 wb.write(fileOut);
-                new Mensaje().showModal(Alert.AlertType.INFORMATION, "Resultado satisfactorio" ,this.getStage(),"Se creó el excel solicitado");
-                
+                new Mensaje().showModal(Alert.AlertType.INFORMATION, "Resultado satisfactorio" ,this.getStage(),"Se creó el excel solicitado");  
             }catch(Exception e) {
                 System.out.println(e.getMessage());
                 new Mensaje().showModal(Alert.AlertType.ERROR, "Falla en archivo" ,this.getStage(),e.getMessage());  
             }
-        } else new Mensaje().showModal(Alert.AlertType.ERROR, "Favor ingrese el dato a buscar" ,this.getStage(),"No se puede crear archivo");
+        }else{
+            new Mensaje().showModal(Alert.AlertType.ERROR, "Favor ingrese el dato a buscar" ,this.getStage(),"No se puede crear archivo");
+        }
         
     }
     public void autoSizeColumns(Workbook workbook) {
@@ -245,6 +225,20 @@ public class mantMarcasController extends Controller implements Initializable {
         }
     }
 }
+    @FXML
+    void onAction_btnBorrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAction_btnGuardar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAction_btnNuevo(ActionEvent event) {
+
+    }
 
     @Override
     public void initialize() {
